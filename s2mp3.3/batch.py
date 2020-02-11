514937eebd12d35986126cef9bfe7d163292418f
Deejideej <athrowawayemailtwo@gmail.com>
2020-02-11 21:53:04 +1100

    Go to sleep

--- s2mp3.3/batch.py
+++ s2mp3.3/batch.py
@@ -52,69 +52,75 @@ for x in range(0, len(input_list)):
         album_vector.append(column[y])
 print(album_vector)
 
-# combine song and lyric lists / main program loop
+# main program loop
+# combine song and lyric lists
 for x in range(0, len(song_vector)):
-    print("downloading song " + str(x+1) + " of " + str(len(song_vector)))
+    print("downloading song " + str(x+1) + " of " + str(len(song_vector))+ ', '+ song_vector[x])
     if not song_vector[x].startswith('#'):
         if (setting_lyric == "y") | (setting_lyric == "Y"):
             song_vector[x] = song_vector[x] + " lyric video"
 
 # search for the video on youtube
-    query = parse.quote(song_vector[x])
-    url = "https://www.youtube.com/results?search_query=" + query
-    response = request.urlopen(url)
-    html = response.read()
-    soup = BeautifulSoup(html, 'html.parser')
-    for vid in soup.findAll(attrs={'class': 'yt-uix-tile-link'}):
-        if ("channel" not in vid['href']) & ("user" not in vid['href']):
-            try:
-                class MyLogger(object):
-                    def debug(self, msg):
-                        pass
-
-                    def warning(self, msg):
-                        pass
-
-                    def error(self, msg):
-                        print(msg)
-
-                def my_hook(d):
-                    if d['status'] == 'finished':
-                        print('Done downloading ' + artist_vector[x].split(',')[0] + " " + song_vector[x] + ', now converting ...')
-
-
-                ydl_opts = {'format': 'bestaudio/best',
-                            'noplaylist': True,
-                            'outtmpl': path.join('output', '%(title)s.%(ext)s'),
-                            'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3',
-                                                'preferredquality': '128', }], 'logger': MyLogger(),
-                            'progress_hooks': [my_hook], }
-                with YoutubeDL(ydl_opts) as ydl:
-                    result = ydl.extract_info(
-                        'https://www.youtube.com' + vid['href'],
-                        download=False  # We just want to extract the info
-                    )
-                    print('downloading ' + result['title'])
-                    print(result['duration'])
-                    name = result['title']
-                    if result['duration'] <= max_seconds:
-                        ydl.download(['https://www.youtube.com' + vid['href']])
-                    else:
-                        print('video too long, skipping')
-            except Exception as e:
-                print("failed to download " + artist_vector[x].split(',')[0] + " " + song_vector[x] + str(e))
-            break
     try:
-        mp3file = MP3('output/' + name + '.mp3', ID3=EasyID3)
-        try:
-            mp3file.add_tags(ID3=EasyID3)
-        except mutagen.id3.error:
-            print("has tags")
-        mp3file['album'] = album_vector[x]
-        mp3file['albumartist'] = album_vector[x]
-        mp3file['artist'] = artist_vector[x]
-        mp3file['title'] = song_vector[x]
-        mp3file.save()  
-    except (FileNotFoundError, mutagen.MutagenError):
-        print("we could not add album metadata to " + name)
+        query = parse.quote(song_vector[x])
+        url = "https://www.youtube.com/results?search_query=" + query
+        response = request.urlopen(url)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        for vid in soup.findAll(attrs={'class': 'yt-uix-tile-link'}):
+            if not vid['href'].startswith("https://googleads.g.doubleclick.net/"):
+                if ("channel" not in vid['href']) & ("user" not in vid['href']):
+                    try:
+                        class MyLogger(object):
+                            def debug(self, msg):
+                                pass
+
+                            def warning(self, msg):
+                                pass
+
+                            def error(self, msg):
+                                print(msg)
+
+                        def my_hook(d):
+                            if d['status'] == 'finished':
+                                print('Done downloading ' + artist_vector[x].split(',')[0] + " " + song_vector[x] + ', now converting ...')
+
+
+                        ydl_opts = {'format': 'bestaudio/best',
+                                    'noplaylist': True,
+                                    'outtmpl': path.join('output', '%(title)s.%(ext)s'),
+                                    'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3',
+                                                        'preferredquality': '128', }], 'logger': MyLogger(),
+                                    'progress_hooks': [my_hook], }
+                        with YoutubeDL(ydl_opts) as ydl:
+                            result = ydl.extract_info(
+                                'https://www.youtube.com' + vid['href'],
+                                download=False  # We just want to extract the info
+                            )
+                            print('downloading ' + result['title'])
+                            print(result['duration'])
+                            name = result['title']
+                            if result['duration'] <= max_seconds:
+                                ydl.download(['https://www.youtube.com' + vid['href']])
+                            else:
+                                print('video too long, skipping')
+                    except Exception as e:
+                        print("failed to download " + artist_vector[x].split(',')[0] + " " + song_vector[x] + str(e))
+                    break
+            try:
+                mp3file = MP3('output/' + name + '.mp3', ID3=EasyID3)
+                try:
+                    mp3file.add_tags(ID3=EasyID3)
+                except mutagen.id3.error:
+                    print("has tags")
+                mp3file['album'] = album_vector[x]
+                mp3file['albumartist'] = album_vector[x]
+                mp3file['artist'] = artist_vector[x]
+                mp3file['title'] = song_vector[x]
+                mp3file.save()  
+            except (FileNotFoundError, mutagen.MutagenError):
+                print("we could not add album metadata to " + name)
+    except OSError:
+        x = x - 1
+        input("There has been an error with the network, press enter to resume")
 
