c547b84fd83f1ae8cee8adbd586b60f8347c2667
Deejideej <athrowawayemailtwo@gmail.com>
2020-03-19 15:00:56 +1100

    Update single.py

--- s2mp3.3/single.py
+++ s2mp3.3/single.py
@@ -3,10 +3,22 @@ from bs4 import BeautifulSoup
 import youtube_dl
 import os
 import shutil
-import time
+import argparse
 
+parser = argparse.ArgumentParser(description='-s [search] -f [format]')
+parser.add_argument('-s', action="store", dest="search")
+parser.add_argument('-f', action="store", dest="format")
+parser.parse_args(['-s', '-f'])
+
+oformat = 0
 directory = os.getcwd()
 
+opts, args = getopt.getopt(argv,"hi:o:",["format="])
+for opt, arg in opts:
+    if opt in ("-f", "--format"):
+        if arg == webm:
+            oformat = 1
+        
 textToSearch = input("Search: ")
 query = urllib.parse.quote(textToSearch)
 url = "https://www.youtube.com/results?search_query=" + query
@@ -29,14 +41,15 @@ for vid in soup.findAll(attrs={'class': 'yt-uix-tile-link'}):
         def my_hook(d):
             if d['status'] == 'finished':
                 print('Done downloading now converting ...')
-
-
-        ydl_opts = {'format': 'bestaudio/best',
-                    'noplaylist': True,
-                    'outtmpl': os.path.join(directory, '%(title)s.%(ext)s'),
-                    'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3',
-                                        'preferredquality': '128', }], 'logger': MyLogger(),
-                    'progress_hooks': [my_hook], }
+        if oformat == 1:
+            ydl_opts = {}
+        else:
+            ydl_opts = {'format': 'bestaudio/best',
+                        'noplaylist': True,
+                        'outtmpl': os.path.join(directory, '%(title)s.%(ext)s'),
+                        'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3',
+                                            'preferredquality': '128', }], 'logger': MyLogger(),
+                        'progress_hooks': [my_hook], }
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             result = ydl.extract_info(
                 'https://www.youtube.com' + vid['href'],
@@ -47,7 +60,6 @@ for vid in soup.findAll(attrs={'class': 'yt-uix-tile-link'}):
                 ydl.download(['https://www.youtube.com' + vid['href']])
             else:
                 print("channel detected, cancelling.")
-                time.sleep(3)
                 break
     except Exception as e:
         print("failed to download " + str(e))
